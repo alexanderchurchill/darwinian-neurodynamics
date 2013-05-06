1ff313878e8304c95ed388b7bd32fbd3aa2c8a2e
Alex <alexanderchurchill@gmail.com>
2013-05-06 21:04:14 +0100

    added config

---
+++ config.py
@@ -0,0 +1,17 @@
+pop_size = 10
+time_step_length = 0.001
+time_steps_per_evaluation = 100
+
+mutation_rate = 0.05
+crossover_rate = 0.05
+
+min_message_delay = 1
+max_message_delay = 10
+
+min_time_active = 1
+max_time_active = 50
+
+min_sensors_in_s_atom = 1
+max_sensors_in_s_atom = 3
+min_motors_in_m_atom = 1
+max_motors_in_m_atom = 4
